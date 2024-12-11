bdebaa333944d605ba1af7c97d91ad819c7c4c7e
shindetejasm <shindetejasm@gmail.com>
2024-12-11 23:55:19 +0530

    merge arrays

--- Java/GFG160/src/com/tejas/day01/SecondLargest.java
+++ Java/GFG160/src/com/tejas/day01/SecondLargest.java
@@ -6,11 +6,7 @@ public class SecondLargest {
 
 	public static void main(String[] args) {
 		int[] arr = { 12, 35, 1, 10, 34, 78, 1 };
-		
-
 		System.out.println(" Second Highest : " + getSecondLargest(arr));
-		
-
 	}
 
 	// Two pointers
@@ -31,25 +27,7 @@ public class SecondLargest {
 		return secondHighest;
 	}
 
-//	public static void putZerosToEnd(int[] arr) {
-//
-//		int latestZeroIndex = arr.length - 1;
-//
-//		for (int i = latestZeroIndex; i > 0; i--) {
-//			if (i != arr.length - 1) {
-//				if (arr[i] == 0 && arr[i + 1] != 0) {
-//					// swap
-//					int temp = arr[latestZeroIndex];
-//					arr[latestZeroIndex] = arr[i];
-//					arr[i] = temp;
-//					latestZeroIndex--;
-//				}
-//			}
-//		}
-//		
-//		System.out.println(Arrays.toString(arr));
-//
-//	}
+
 	
 
 }
