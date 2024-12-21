f74f15f0d6eb286e664242833f2eef3febbcf181
shindetejasm <shindetejasm@gmail.com>
2024-12-21 21:18:27 +0530

    gfg 35 | matrix rotation by 90 degree anti clockwise

---
+++ Java/GFG160/src/com/tejas/day035/RotateMatrix90Deg.java
@@ -0,0 +1,61 @@
+package com.tejas.day035;
+
+import java.util.Arrays;
+
+public class RotateMatrix90Deg {
+	public static void main(String arg[]) {
+		int[][] arr = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+
+//		rotateArray(arr);
+		rotateArrayWithoutExtraSpace(arr);
+
+//		for (int[] a : arr) {
+//			System.out.println(Arrays.toString(a));
+//		}
+
+	}
+
+	private static void rotateArrayWithoutExtraSpace(int[][] mat) {
+		int n = mat.length;
+
+		for (int i = 0; i <= n - 1; i++) {
+			for (int j = 0; j <= n - 1; j++) {
+				if (i < j) {
+					int temp = mat[i][j];
+					mat[i][j] = mat[j][i];
+					mat[j][i] = temp;
+				}
+			}
+		}
+
+		for (int i = 0; i < n / 2; i++) {
+			for (int j = 0; j <= n - 1; j++) {
+				int temp = mat[i][j];
+				mat[i][j] = mat[n - 1 - i][j];
+				mat[n - 1 - i][j] = temp;
+			}
+		}
+		for (int[] a : mat) {
+			System.out.println(Arrays.toString(a));
+		}
+
+	}
+
+	// Using extra space
+	private static void rotateArray(int[][] arr) {
+
+		int rows = arr.length;
+		int cols = arr[0].length;
+		int[][] newArr = new int[rows][cols];
+
+		for (int i = cols - 1; i >= 0; i--) {
+
+			for (int j = 0; j <= rows - 1; j++) {
+				newArr[cols - 1 - i][j] = arr[j][i];
+			}
+		}
+		for (int[] a : newArr) {
+			System.out.println(Arrays.toString(a));
+		}
+	}
+}
