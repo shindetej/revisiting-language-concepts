da0788ff45830e446b52d18e308d99a39275aec7
shindetej <shindetejasm@gmail.com>
2025-02-21 23:59:28 +0530

    GFG 98 | stack paranthesis

---
+++ Java/GFG160/src/com/tejas/day098/ParanthesisChecker.java
@@ -0,0 +1,32 @@
+package com.tejas.day098;
+
+import java.util.Stack;
+
+public class ParanthesisChecker {
+	public static void main(String[] args) {
+		System.out.println("IS BALANCED : "+ isBalanced("{[]}"));
+		System.out.println("IS BALANCED : "+ isBalanced("{[][}"));
+		System.out.println("IS BALANCED : "+ isBalanced("{()[]}"));
+	}
+
+	static boolean isBalanced(String s) {
+		Stack<Character> stack = new Stack<>();
+
+		for (char c : s.toCharArray()) {
+			if (c == '(' || c == '{' || c == '[') {
+				stack.push(c);
+			} else {
+				if (stack.isEmpty())
+					return false;
+
+				char top = stack.pop();
+
+				if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+					return false;
+				}
+			}
+
+		}
+		return stack.isEmpty();
+	}
+}
